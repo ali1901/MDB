4e1df799891bdc5278c0255a8d3fd040abf02893
Ali Safari <alisafari@Alis-Mac.local>
2021-08-24 09:08:56 -0700

    after implementation of OmdbApi file and UrlSession

---
+++ MDB/Controllers/MovieDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailViewController.swift
+//  MDB
+//
+//  Created by Ali Safari on 8/24/21.
+//  Copyright © 2021 Ali Safari. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MDB/Models/OMDBApi.swift
@@ -0,0 +1,9 @@
+//
+//  OMDBApi.swift
+//  MDB
+//
+//  Created by Ali Safari on 8/24/21.
+//  Copyright © 2021 Ali Safari. All rights reserved.
+//
+
+import Foundation
