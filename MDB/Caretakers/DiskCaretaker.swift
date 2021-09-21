f4477cd254e5dfafd13ad43cfa6cba4da8a32886
Ali Safari <alisafari@Alis-Mac.local>
2021-09-21 09:32:01 -0700

    Added Caretaker file to project.

---
+++ MDB/Caretakers/DiskCaretaker.swift
@@ -0,0 +1,61 @@
+//
+//  DiskCaretaker.swift
+//  MDB
+//
+//  Created by Ali Safari on 9/21/21.
+//  Copyright © 2021 Ali Safari. All rights reserved.
+//
+
+import Foundation
+
+class DiskCaretaker {
+    
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    
+    private func objURL(for key: String) -> URL {
+        let directories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let directory = directories.first!
+        
+        return directory.appendingPathComponent("\(key)")
+    }
+    
+    func save<T: Codable>(theObject object: T, withName name: String) throws {
+        let url = objURL(for: name)
+        do {
+            let data = try encoder.encode(object)
+            try data.write(to: url, options: .atomic)
+        } catch (let error) {
+            print("Error saving data to disk: \(error).")
+        }
+    }
+    
+    func load<T: Codable>(toObject object: T.Type, withName name: String) throws -> T? {
+        let url = objURL(for: name)
+        do {
+            let data = try Data(contentsOf: url)
+            return try decoder.decode(object.self, from: data)
+        } catch {
+            print("Error loading data from disk: \(error).")
+            return nil
+        }
+        
+    }
+    
+//    public static func retrieve<T: Codable>(_ type: T.Type, from fileName: String) throws -> T {
+//        let url = objURL(fileName)
+//        return try retrieve(T.self, from: url)
+//    }
+//    // 2
+//    public static func retrieve<T: Codable>(_ type: T.Type, from url: URL) throws -> T {
+//        do {
+//            // 3
+//            let data = try Data(contentsOf: url)
+//            // 4
+//            return try decoder.decode(T.self, from: data)
+//        } catch (let error) {
+//            // 5
+//            print("Retrieve failed: URL: `\(url)`, Error: `\(error)`")
+//            throw error }
+//    }
+}
